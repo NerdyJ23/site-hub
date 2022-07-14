b9d70e609d34d9a3ee26124abeca685cf7884088
= <=>
2022-07-13 18:14:04 -1000

    initial

---
+++ index.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" type="text/css" href="/css/style.css" />
+
+<h1>Collection of stuff I've created </h1>
+
+<div id="sites">
+  <a href="https://warframe.jessprogramming.freeddns.org" class='site' style='background-image:url("/img/warframe.jpg")'>
+    <div class='fade'>
+      Warframe Checklist
+    </div>
+  </a>
+  <a href="https://budget.jessprogramming.freeddns.org" class='site' style='background-image:url("/img/budgeting.jpg")'>
+    <div class='fade'>
+      Budgeting Form
+    </div>
+  </a>
+  <a href="https://project.jessprogramming.freeddns.org/projects_dir/list/" class='site' style='background-image:url("/img/todo.jpg")'>
+    <div class='fade'>
+      ToDo List
+    </div>
+  </a>
+  <a href="https://newdnd.jessprogramming.freeddns.org" class='site' style='background-image:url("/img/dnd.jpg")'>
+    <div class='fade'>
+      Dnd Character Sheets
+    </div>
+  </a>
+</div>
